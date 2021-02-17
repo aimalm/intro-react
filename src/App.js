95d3800c01853067a91e246c5b73090ea6d4cc12
Aimal-Maarij <51042919+Aimal-Maarij@users.noreply.github.com>
2021-02-17 09:36:51 +0100

    WIP add todo feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
+import TodoList from './components/TodoList';
+import { v4 as uuidv4 } from 'uuid';
+
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef();
+
+  function handleAddTodo(e){
+    const name = todoNameRef.current.value
+    
+    if(name === '') return
+      setTodos(prevTodos => {
+        return [...prevTodos, {id:uuidv4(), name: name, complete: false}]
+      })
+      todoNameRef.current.value = null
+    
+  
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoList todos={todos} />
+
+      
+      <input type="text" ref={todoNameRef}></input>
+      <button onClick = {handleAddTodo}>Add Todo</button>
+      <button>Clear Todo</button>
+      <div> 0 left to do</div>
+
+    </>
   );
 }
 
