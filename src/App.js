98caa99992092fb664bad8e44db259602b1b5452
Aimal-Maarij <51042919+Aimal-Maarij@users.noreply.github.com>
2021-02-18 10:51:39 +0100

    adding all features + styling

--- src/App.js
+++ src/App.js
@@ -1,38 +1,63 @@
 //import logo from './logo.svg';
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './components/TodoList';
 import { v4 as uuidv4 } from 'uuid';
 
-import './App.css';
+var date = new Date();
+
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
   const [todos, setTodos] = useState([])
-  const todoNameRef = useRef();
+  const todoNameRef = useRef()
 
-  function handleAddTodo(e){
-    const name = todoNameRef.current.value
-    
-    if(name === '') return
-      setTodos(prevTodos => {
-        return [...prevTodos, {id:uuidv4(), name: name, complete: false}]
-      })
-      todoNameRef.current.value = null
-    
-  
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
 
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
   }
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
+
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
   return (
     <>
-      <TodoList todos={todos} />
-
-      
-      <input type="text" ref={todoNameRef}></input>
-      <button onClick = {handleAddTodo}>Add Todo</button>
-      <button>Clear Todo</button>
-      <div> 0 left to do</div>
+      <div class="container" >
+        <p>{date.toDateString()}</p>
+        <h2>My Todo List</h2>
+        <div class="todo_btn_container">
+          <input ref={todoNameRef} type="text" />
+          <button class="btn btn-primary" onClick={handleAddTodo}>Add Todo</button>
+          <button class="btn btn-danger" onClick={handleClearTodos}>Clear Complete</button>
+        </div>
+        <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+        <TodoList todos={todos} toggleTodo={toggleTodo} />
+      </div>
 
     </>
-  );
+  )
 }
 
 export default App;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
-export default function Todo({todo}) {
-    return (
-        <div>
-            <label>
-                <input type="checkbox" checked= {todo.complete}></input>
-
-            {todo.name}
-
-            </label>
-        </div>
-       
-    )
+export default function Todo({ todo, toggleTodo }) {
+  function handleTodoClick() {
+    toggleTodo(todo.id)
+  }
+  
+  return (
+    <div class="container todo">
+      <label>
+        <input  type="checkbox" checked={todo.complete} onChange={handleTodoClick} />
+        <p>{todo.name}</p>
+      </label>
+    </div>
+  )
 }
